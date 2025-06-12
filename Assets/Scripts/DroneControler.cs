2fae0559c0d5aac7df0e9e16f9efa2e1b6f4805c
MARXstoodin <abramovarsenii45@gmail.com>
2025-06-12 23:02:48 +0300

    drone movement improvement

--- Assets/Scripts/DroneControler.cs
+++ Assets/Scripts/DroneControler.cs
@@ -6,55 +6,66 @@ using UnityEngine;
 public class DroneControler : MonoBehaviour
 {
     public float speed;
-    public float drag;
+    public float turnSeed;
     public GameObject drone;
-    private Vector3 velocity;
-    private float force = 0;
-    private float zTurn = 0;
-    private float forceUp = 0;
+    private Vector3 velocity = new Vector3(0f,0f,0f);
+    private int force = 0;
+    private int turn = 0;
+    private int forceSide = 0;
+    private int forceUp = 0;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            force += 0.01f;
+            force = 1;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            force -= 0.01f;
+            force = -1;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            zTurn += 0.003f;
+            forceSide = 1;
         }
-        
+
         if (Input.GetKey(KeyCode.D))
         {
-            zTurn -= 0.003f;
+            forceSide = -1;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            turn = 1;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            turn = -1;
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            forceUp += 0.01f;
+            forceUp = 1;
         }
 
         if (Input.GetKey(KeyCode.C))
         {
-            forceUp -= 0.01f;
+            forceUp = -1;
         }
 
-        force -= math.pow(force, 3) / drag + force / drag;
-        forceUp -= math.pow(forceUp, 3) / drag + forceUp / drag;
+        velocity.x = force * (2 * drone.transform.rotation.y * drone.transform.rotation.w);//x = f * 2sinAcosA; f = {-1,0,1}  сила по оси x
+        velocity.z = force * (math.pow(drone.transform.rotation.w, 2) - math.pow(drone.transform.rotation.y, 2));//z = f * cos^2A-sin^2A; f = {-1,0,1}  сила по оси z
+        //velocity.y = forceUp;
 
-        velocity.x = force * math.cos(zTurn) * math.sqrt(math.cos(forceUp)-0.06f);
-        velocity.z = force * math.sin(zTurn) * math.sqrt(math.cos(forceUp)-0.06f);
-        velocity.y = forceUp * math.sqrt(math.cos(force)-0.06f);
+        drone.GetComponent<Rigidbody>().AddForce(velocity * speed * Time.deltaTime);
+        drone.GetComponent<Rigidbody>().AddTorque(new Vector3(0, turn * turnSeed, 0) * Time.deltaTime);
 
-        print((math.abs(velocity.x) + math.abs(velocity.y) + math.abs(velocity.z)) / 3);
-
-        drone.transform.rotation = new Quaternion(0, math.cos(zTurn/2) * Time.deltaTime, 0, math.sin(zTurn/2) * Time.deltaTime);
-        drone.transform.position += velocity * speed * Time.deltaTime;
+        force = 0;
+        forceUp = 0;
+        forceSide = 0;
+        turn = 0;
     }
 }
