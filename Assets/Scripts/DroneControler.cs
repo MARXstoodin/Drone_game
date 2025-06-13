245c0cabf2da67d648ceda965a0344dc70576ea0
MARXstoodin <abramovarsenii45@gmail.com>
2025-06-13 23:41:54 +0300

    movement done

--- Assets/Scripts/DroneControler.cs
+++ Assets/Scripts/DroneControler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -7,12 +8,17 @@ public class DroneControler : MonoBehaviour
 {
     public float speed;
     public float turnSeed;
+    public float camSens;
     public GameObject drone;
-    private Vector3 velocity = new Vector3(0f,0f,0f);
+    private Vector3 velocity = new Vector3(0f, 0f, 0f);
+    private Vector3 mousePos = new Vector3(Screen.height/2, Screen.width/2, 0f);
     private int force = 0;
-    private int turn = 0;
     private int forceSide = 0;
     private int forceUp = 0;
+    private float turn = 0;
+    private float sin2x;
+    private float cos2x;
+    private float forceModifier = 0;
 
     void Update()
     {
@@ -36,16 +42,6 @@ public class DroneControler : MonoBehaviour
             forceSide = -1;
         }
 
-        if (Input.GetKey(KeyCode.E))
-        {
-            turn = 1;
-        }
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            turn = -1;
-        }
-
         if (Input.GetKey(KeyCode.Space))
         {
             forceUp = 1;
@@ -56,9 +52,24 @@ public class DroneControler : MonoBehaviour
             forceUp = -1;
         }
 
-        velocity.x = force * (2 * drone.transform.rotation.y * drone.transform.rotation.w);//x = f * 2sinAcosA; f = {-1,0,1}  сила по оси x
-        velocity.z = force * (math.pow(drone.transform.rotation.w, 2) - math.pow(drone.transform.rotation.y, 2));//z = f * cos^2A-sin^2A; f = {-1,0,1}  сила по оси z
-        //velocity.y = forceUp;
+        mousePos = Input.mousePosition - mousePos;
+        print(mousePos);
+        mousePos = new Vector3(transform.eulerAngles.x - mousePos.y, transform.eulerAngles.y + mousePos.x, 0);
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, mousePos, 0.2f);
+        mousePos = Input.mousePosition;
+
+        sin2x = 2 * drone.transform.rotation.y * drone.transform.rotation.w;// 2sinAcosA;  вектор силы по локальной оси x
+        cos2x = math.pow(drone.transform.rotation.w, 2) - math.pow(drone.transform.rotation.y, 2);// cos^2A-sin^2A;  вектор силы по локальной оси z
+        forceModifier = 0 + math.sqrt(math.abs(force) + math.abs(forceUp) + math.abs(forceSide)) / (math.abs(force) + math.abs(forceUp) + math.abs(forceSide));// модуль векторов
+        if (float.IsNaN(forceModifier)) {
+            forceModifier = 0;
+        }
+
+        velocity.x = force * forceModifier * sin2x + forceSide * forceModifier * -cos2x;
+        velocity.z = force * forceModifier * cos2x + forceSide * forceModifier * sin2x;
+        velocity.y = forceUp * forceModifier;
+
+        //print(math.sqrt(math.pow(velocity.x, 2) + math.pow(velocity.z, 2) + math.pow(velocity.y, 2)));
 
         drone.GetComponent<Rigidbody>().AddForce(velocity * speed * Time.deltaTime);
         drone.GetComponent<Rigidbody>().AddTorque(new Vector3(0, turn * turnSeed, 0) * Time.deltaTime);
